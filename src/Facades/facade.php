222dc9be673be6e522d86423c79fbff4bac12a0d
Lukas OTERO--MELEDO <lukas@projectheberg.com>
2024-04-09 00:29:29 +0200

    Added facade: view Twig file for view

---
+++ src/Facades/facade.php
@@ -0,0 +1,20 @@
+<?php
+
+use Bloom\App;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+/**
+ * @throws \Twig\Error\SyntaxError
+ * @throws \Twig\Error\RuntimeError
+ * @throws \Twig\Error\LoaderError
+ */
+function view(string $viewName, array $parameters): void
+{
+    $loader = new FilesystemLoader(App::dir().'/resources/views');
+    $twig = new Environment($loader);
+
+    echo $twig->render($viewName, $parameters);
+}
